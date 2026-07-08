bc27122c64305b4732402ed7f730d71a0eb7476e
jiyeoniii <dlwldus0705@naver.com>
2026-07-09 04:19:02 +0900

    chore: 미사용 accounts 앱 삭제

--- piro25_Pirostagram/config/urls.py
+++ piro25_Pirostagram/config/urls.py
@@ -21,7 +21,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('accounts.urls')),
     path('', include('posts.urls')),
 ]
 
